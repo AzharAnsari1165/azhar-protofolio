5983b9f9d770334633949c579fc46443d005dc6f
AzharAnsari1165 <149761572+AzharAnsari1165@users.noreply.github.com>
2026-03-19 17:18:12 +0500

    odad

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/mywebsite/",
+  base: '/azhar-protofolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
